f690c74afc414122385ea15bd67d5fa60cc68a4a
Bára <bara.pospisilova.024@pslib.cz>
2025-05-20 08:26:50 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -32,3 +32,39 @@ basic.forever(function() {
 
     PCAmotor.MotorRun(PCAmotor.Motors.M4, P * -1)
 })
+
+radio.setGroup(234) //tomáš
+radio.setGroup(37) //roman
+radio.setGroup(1) //tonda
+radio.setGroup(77) //tobi
+radio.setGroup(253) //marek
+radio.setGroup(15) //vilém
+radio.setGroup(21) //martin
+radio.setGroup(173) //kuba
+radio.setGroup(12) //roman
+radio.setGroup(23) //jára
+radio.setGroup(22) //martin
+radio.setTransmitPower(2)
+radio.setFrequencyBand(50)
+ 
+basic.forever(function () {
+    let x = input.acceleration(Dimension.X)
+    let y = input.acceleration(Dimension.Y)
+ 
+    let speedX = Math.map(x, -1024, 1024, -250, 250)
+    let speedY = Math.map(y, -1024, 1024, -250, 250)
+ 
+    let leftMotor = speedY - speedX
+    let rightMotor = speedY + speedX
+ 
+    leftMotor = Math.round(leftMotor)
+    rightMotor = Math.round(rightMotor)
+ 
+    leftMotor = Math.constrain(leftMotor, 0, 0)
+    rightMotor = Math.constrain(rightMotor, 0, 0)
+ 
+    let zprava = "S|" + leftMotor + "|" + rightMotor
+ 
+    radio.sendString(zprava)
+ 
+})
