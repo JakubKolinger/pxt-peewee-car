5cc94f910ed10704f9c78de92b994f4534905533
Bára <bara.pospisilova.024@pslib.cz>
2025-05-20 08:38:43 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -49,19 +49,8 @@ radio.setTransmitPower(2)
 radio.setFrequencyBand(50)
  
 basic.forever(function () {
-    let x = input.acceleration(Dimension.X)
-    let y = input.acceleration(Dimension.Y)
  
-    let speedX = Math.map(x, -1024, 1024, -250, 250)
-    let speedY = Math.map(y, -1024, 1024, -250, 250)
- 
-    let leftMotor = speedY - speedX
-    let rightMotor = speedY + speedX
- 
-    leftMotor = Math.round(leftMotor)
-    rightMotor = Math.round(rightMotor)
- 
-    leftMotor = Math.constrain(leftMotor, 0, 250)
+  leftMotor = Math.constrain(leftMotor, 0, 250)
     rightMotor = Math.constrain(rightMotor, 0, 0)
  
     let zprava = "S|" + leftMotor + "|" + rightMotor
