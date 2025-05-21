3870f93d7fd12a3fcb603b1e1b9966ba9ad59746
JakubKolinger <jakub.kolinger.024@pslib.cz>
2025-05-21 18:59:09 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -7,18 +7,22 @@ let servo = ServoHelper.createServo()
 
 let L: number = 0;
 let P: number = 0;
+let speed: number = 0;
+let speed1: number = 0;
+let speedLED: number = 0;
 
 
 
 radio.onReceivedString(function (received) {
     let casti = received.split("|")
 
-    if (casti.length === 3 && casti[0] === "S") {
+    if (casti.length === 4 && casti[0] === "S") {
         let serial: number = radio.receivedPacket(RadioPacketProperty.SerialNumber)
 
         if (serial === -978678300) {
             L = parseInt(casti[1])
             P = parseInt(casti[2])
+            speed = parseInt(casti[3])
         }
             
             
@@ -43,17 +47,23 @@ basic.forever(function() {
     let strip1 = neopixel.create(DigitalPin.P0, 9, NeoPixelMode.RGB)
     let strip2 = neopixel.create(DigitalPin.P16, 9, NeoPixelMode.RGB)
 
-    if (L < P){
-        strip1.setPixelColor(0, neopixel.colors(NeoPixelColors.Orange))
-        strip1.setPixelColor(1, neopixel.colors(NeoPixelColors.Orange))
-        strip1.setPixelColor(2, neopixel.colors(NeoPixelColors.Orange))
+    speed1 = Math.map(speed, -250, 250, -4, 4)
+    speed1 = Math.round(speed1)
+    speedLED = Math.abs(speed1)
+    if (speedLED === 1){
+        strip2.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
     }
-    if (P < L){
-        strip1.setPixelColor(6, neopixel.colors(NeoPixelColors.Orange))
-        strip1.setPixelColor(7, neopixel.colors(NeoPixelColors.Orange))
-        strip1.setPixelColor(8, neopixel.colors(NeoPixelColors.Orange))
+    if (speedLED === 2){
+        strip2.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
     }
-    
-    strip1.show()
+    if (speedLED === 3) {
+        strip2.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+    }
+    if (speedLED === 4) {
+        strip2.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
+    }
 })
+
+
+
 
